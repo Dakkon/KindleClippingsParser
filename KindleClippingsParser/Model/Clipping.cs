7751ce7ebc65e99c6b10674718d314d4aca44fc9
Dakkon <radoslaw.piwko@gmail.com>
2013-12-14 11:28:40 +0100

    Model classes skeleton

---
+++ KindleClippingsParser/Model/Clipping.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace KindleClippingsParser.Model
+{
+    class Clipping
+    {
+        #region Private fields
+
+        private string m_Title;
+        private string m_Author;
+        private DateTime m_Timestamp;
+        private string m_Text;
+
+        bool m_IsEnabled;
+
+        #endregion Private fields
+        #region Properties
+
+        /// <summary>
+        /// Clipping source title (book title, article title etc.)
+        /// </summary>
+        public string Title
+        {
+            get
+            {
+                return this.m_Title;
+            }
+        }
+
+        /// <summary>
+        /// Clipping author
+        /// </summary>
+        public string Author
+        {
+            get
+            {
+                return this.m_Author;
+            }
+        }
+
+        /// <summary>
+        /// Time, when the clipping was taken
+        /// </summary>
+        public DateTime Timestamp
+        {
+            get
+            {
+                return this.m_Timestamp;
+            }
+        }
+
+        /// <summary>
+        /// Clipping text
+        /// </summary>
+        public string Text
+        {
+            get
+            {
+                return this.m_Text;
+            }
+        }
+
+        /// <summary>
+        /// Flag which determine whether clipping:
+        /// - is visible to the user (used e.g. for filtering)
+        /// - will be saved when user decides to update his clipping file
+        /// Default value is true
+        /// </summary>
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.m_IsEnabled;
+            }
+
+            set
+            {
+                this.m_IsEnabled = value;
+            }
+        }
+
+        #endregion Properties
+        #region Ctors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Clipping()
+        {
+            this.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// Constructor which initilalizes all clipping's properties
+        /// </summary>
+        /// <param name="title">Clipping source title (book title, article title etc.)</param>
+        /// <param name="author">Clipping author</param>
+        /// <param name="timeStamp">Time, when the clipping was taken</param>
+        /// <param name="text">Clipping text</param>
+        public Clipping(string title, string author, string ts, string text) 
+            : this()
+        {
+            this.SetTitle(title);
+            this.SetAuthor(author);
+            this.SetTimeStamp(ts);
+            this.SetText(text);            
+        }
+
+        #endregion Ctors
+        #region Public methods
+
+        /// <summary>
+        /// Sets clipping title
+        /// </summary>
+        /// <param name="title">Clipping source title (book title, article title etc.)</param>
+        public void SetTitle(string title)
+        {
+            this.m_Title = title;
+        }
+
+        /// <summary>
+        /// Sets clipping author
+        /// </summary>
+        /// <param name="author">Clipping author</param>
+        public void SetAuthor(string author)
+        {
+            this.m_Author = author;
+        }
+
+        /// <summary>
+        /// Sets clipping timestamp
+        /// </summary>
+        /// <param name="timeStamp">Time, when the clipping was taken</param>
+        public void SetTimeStamp(string ts)
+        {            
+            throw (new NotImplementedException());            
+        }
+
+        /// <summary>
+        /// Sets clipping text
+        /// </summary>
+        /// <param name="text">Clipping text</param>
+        public void SetText(string text)
+        {
+            this.m_Text = text;
+        }
+
+        #endregion Public methods
+    }
+}
---
+++ KindleClippingsParser/Model/ClippingsFileParser.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KindleClippingsParser.Model
+{
+    class ClippingsFileParser
+    {
+        #region Private fields
+
+        private string m_PathToClippingsFile;
+        private List<Clipping> m_ListOfMyClippings;        
+        
+        #endregion Private fields
+        #region Properties
+
+        /// <summary>
+        /// List of clipping objects generated from MyClippings.txt file
+        /// </summary>
+        public List<Clipping> ListOfMyClippings
+        {
+            get
+            {
+                return this.m_ListOfMyClippings;
+            }
+        }
+
+        #endregion Properties
+        #region Ctors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="pathToClippingsFile">Full path to MyClippings.txt file (folders + file name)</param>
+        public ClippingsFileParser(string pathToClippingsFile)
+        {
+            this.m_PathToClippingsFile = pathToClippingsFile;
+        }
+
+        #endregion Ctors
+        #region Public methods
+
+        /// <summary>
+        /// Gets list of all titles found in MyClippings.txt (without duplicates)
+        /// </summary>
+        /// <returns>List of clipping titles</returns>
+        public List<string> GetListOfTitles()
+        {
+            throw (new NotImplementedException());
+        }
+
+        /// <summary>
+        /// Gets list of all authors found in MyClippings.txt (without duplicates)
+        /// </summary>
+        /// <returns>List of clipping authors</returns>
+        public List<string> GetListOfAuthors()
+        {
+            throw (new NotImplementedException());
+        }
+
+        /// <summary>
+        /// Filters list of clippings by title (only clippings with given title will be shown)
+        /// </summary>
+        /// <param name="title">Title to be found</param>
+        public void FilterListByTitle(string title)
+        {
+            throw (new NotImplementedException());
+        }
+
+        /// <summary>
+        /// Filters list of clippings by author (only clippings with given author will be shown)
+        /// </summary>
+        /// <param name="author">Author to be found</param>
+        public void FilterListByAuthor(string author)
+        {
+            throw (new NotImplementedException());
+        }
+
+        /// <summary>
+        /// Filters list of clippings by timestamp (only clippings with given timestamp will be shown)
+        /// </summary>
+        /// <param name="ts">Timestamp to be found</param>
+        public void FilterListByTimestamp(DateTime ts)
+        {
+            throw (new NotImplementedException());
+        }
+
+        /// <summary>
+        /// Filters list of clippings by timestamp
+        /// </summary>
+        /// <param name="ts">Only older clippings will be shown</param>
+        /// <param name="showEqual">Show also clippings with timestamp equal to ts</param>
+        public void FilterOlderThan(DateTime ts, bool showEqual)
+        {
+            throw (new NotImplementedException());
+        }
+
+        /// <summary>
+        /// Filters list of clippings by timestamp
+        /// </summary>
+        /// <param name="ts">Only newer clippings will be shown</param>
+        /// <param name = "showEqual">Show also clippings with timestamp equal to ts</param>
+        public void FilterNewerThan(DateTime ts, bool showEqual)
+        {
+            throw (new NotImplementedException());
+        }
+
+        #endregion Public methods
+    }
+}
